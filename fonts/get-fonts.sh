83678fbbb1245195b2102d203ecf602e7d1d0f51
Mateusz Adamowski <mateusz@adamowski.pl>
2018-08-06 19:59:04 +0200

    wstępna wersja

---
+++ fonts/get-fonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+wget https://dl.dafont.com/dl/?f=usaaf_stencil -O usaaf.zip
+unzip usaaf.zip USAAF_Stencil.ttf
+mv USAAF_Stencil.ttf USAAF.ttf
+
+wget https://dl.dafont.com/dl/?f=rh_carrier_stencil -O rhcas.zip
+unzip rhcas.zip RHCAS___.TTF
+mv RHCAS___.TTF RHCAS.ttf
+
